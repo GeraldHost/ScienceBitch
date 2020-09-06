2f9f98ed846da1f14c955743669a1223be46ff61
Gerald <me@jacobford.co.uk>
2020-09-06 09:48:48 +0100

    feat(shakespear)

---
+++ shakespeare/main.py
@@ -0,0 +1,133 @@
+import tensorflow as tf
+
+import numpy as np
+import os
+import time
+
+path_to_file = './shakespeare.txt'
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+print ('Length of text: {} characters'.format(len(text)))
+
+vocab = sorted(set(text))
+print ('{} unique characters'.format(len(vocab)))
+
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+text_as_int = np.array([char2idx[c] for c in text])
+
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
+
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+def split_input_target(chunk):
+  input_text = chunk[:-1]
+  target_text = chunk[1:]
+  return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+# Batch size
+BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+vocab_size = len(vocab)
+embedding_dim = 256
+rnn_units = 1024
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+
+model = build_model(
+    vocab_size = len(vocab),
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units,
+    batch_size=BATCH_SIZE)
+
+model.summary()
+
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+model.compile(optimizer='adam', loss=loss)
+
+# Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+EPOCHS=10
+#history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+
+model.build(tf.TensorShape([1, None]))
+
+model.summary()
+
+def generate_text(model, start_string):
+  # Evaluation step (generating text using the learned model)
+
+  # Number of characters to generate
+  num_generate = 1000
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+
+  # Low temperatures results in more predictable text.
+  # Higher temperatures results in more surprising text.
+  # Experiment to find the best setting.
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+    predictions = model(input_eval)
+    # remove the batch dimension
+    predictions = tf.squeeze(predictions, 0)
+    print("PRED %s" % predictions)
+
+    # using a categorical distribution to predict the character returned by the model
+    predictions = predictions / temperature
+    # predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+    predicted_id = tf.argmax(predictions).numpy()
+
+    # We pass the predicted character as the next input to the model
+    # along with the previous hidden state
+    input_eval = predicted_id
+    # input_eval = tf.expand_dims([predicted_id], 0)
+
+    text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+
+print(generate_text(model, start_string=u"ROMEO: "))
